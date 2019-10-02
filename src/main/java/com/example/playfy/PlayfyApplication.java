1c32a828f33f973f55e6bbbb04ab65d566b4cc10
JaviBL8 <javibl8@correo.ugr.es>
2019-10-02 16:09:59 +0200

    Hola mundo Spring Boot. #11

---
+++ src/main/java/com/example/playfy/PlayfyApplication.java
@@ -0,0 +1,13 @@
+package com.example.playfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayfyApplication.class, args);
+	}
+
+}
