0a8973c0375e9bd3a0a40db02298420007225845
JaviBL8 <javibl8@correo.ugr.es>
2019-10-25 11:19:12 +0200

    Fin errores API. Closes #21

--- src/main/java/com/playfy/exception/ResourceNotFoundException.java
+++ src/main/java/com/playfy/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception{
 
-	public ResourceNotFoundException(String message){
+	public ResourceNotFoundException(String message) {
         super(message);
     }
 }
